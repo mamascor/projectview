35721332ca9f0d21c2f3b314f3392406778e12b2
Marco Mascorro <marco.mascorroa@icloud.com>
2023-07-22 12:14:50 -0500

    Initial Commit

---
+++ pv/pvApp.swift
@@ -0,0 +1,17 @@
+//
+//  pvApp.swift
+//  pv
+//
+//  Created by Marco Mascorro on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct pvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
